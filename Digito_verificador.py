859ee0d10e4bf0e585e1ec6bda77d399ac6996fd
MariCifu <152333294+Maricifu@users.noreply.github.com>
2023-12-28 21:35:32 -0500

    verificar

--- Digito_verificador.py
+++ Digito_verificador.py
@@ -1,1 +1,18 @@
-2
+def Reversed_rol (rol):
+    lista_digitos = [int(digito) for digito in str(rol)]
+    lista_digitos.reverse()
+    numero_inverso = int(''.join(map(str, lista_digitos)))
+    return numero_inverso
+
+
+while True:
+    try:
+        rol= str(input("Ingrese el rol UTFSM: "))
+        break
+    except ValueError:
+        print("Ingrese correctamente el rol UTFSM: ")
+
+    
+
+
+
--- Digitos.py
+++ Digitos.py
@@ -1,2 +1,4 @@
-1
+num= int(input("Ingrese un número entero: "))
+cantdigitos= len(str(abs(num)))
 
+print(f"{num} tiene {cantdigitos} dígitos")
