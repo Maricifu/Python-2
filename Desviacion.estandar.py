0af3ad0afd3b70272c1ce4935e1078d0f02f388d
MariCifu <152333294+Maricifu@users.noreply.github.com>
2024-01-04 15:58:58 -0500

    Listas

--- Desviacion.estandar.py
+++ Desviacion.estandar.py
@@ -1,1 +1,25 @@
-hi
+import math
+
+def desviacion_estandar(valores):
+    
+    promedio = sum(valores) / len(valores)
+
+    suma_cuadrados_diferencias = sum((x - promedio) ** 2 for x in valores)
+
+    varianza = suma_cuadrados_diferencias / len(valores)
+
+    desviacion_estandar = math.sqrt(varianza)
+
+    return desviacion_estandar
+
+valores_ingresados = input("Ingrese los valores separados por espacios: ")
+valores = [float(valor) for valor in valores_ingresados.split()]
+
+if len(valores) < 2:
+    print("Debe ingresar al menos dos valores para calcular la desviación estándar.")
+else:
+    resultado = desviacion_estandar(valores)
+    print(f"La desviación estándar es: {resultado:.2f}")
+
+
+    
